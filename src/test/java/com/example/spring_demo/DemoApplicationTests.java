92ef8d4d96bc07a338ee98cec965cd64436dab3f
Krishna Sai Aila <161733492+richglitch7@users.noreply.github.com>
2025-12-04 09:34:50 +0000

    add ci.yml workflow

---
+++ src/test/java/com/example/spring_demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTests {
+
+    @Test
+    void testHello() {
+        DemoApplication app = new DemoApplication();
+        assertEquals("Hello World!", app.hello());
+    }
+}
